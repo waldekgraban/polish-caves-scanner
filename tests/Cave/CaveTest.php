1bfbef263c53dee7da8bd749f2ac572d264b2eb0
Waldemar Graban <waldekgraban@gmail.com>
2020-03-11 13:28:29 +0100

    Add some tests of Cave class seter and geter methods

--- tests/Cave/CaveTest.php
+++ tests/Cave/CaveTest.php
@@ -47,6 +47,207 @@ class CaveTest extends TestCase
         $this->cave = new Cave($name, $other_names, $inventory_number, $region, $coordinates_wgs84, $community, $county, $voivodeship, $owner, $basis_of_protection, $hole_exposure, $other_holes, $absolute_height, $relative_height, $depth, $exceeds, $drop, $length, $horizontal_extension, $geographical_location, $description_of_access, $description, $research_history, $exploration_history, $documentation_history, $status, $literature, $study_authors, $editorial, $condition_by_year, $link_cbdg, $cbdg_number);
     }
 
+    public function testCanInitializeByConstructor()
+    {
+        $this->assertInstanceOf(Cave::class, $this->cave);
+    }
+
+    public function testSetName()
+    {
+        $data = " new name";
+        $this->cave->setName($data);
+        $this->assertSame($this->cave->getName($data), trim($data));
+    }
+
+    public function testSetOtherNames()
+    {
+        $data = " new other name";
+        $this->cave->setOtherNames($data);
+        $this->assertSame($this->cave->getOtherNames($data), trim($data));
+    }
+
+    public function testSetInventoryNumber()
+    {
+        $data = " 0010";
+        $this->cave->setInventoryNumber($data);
+        $this->assertSame($this->cave->getInventoryNumber($data), trim($data));
+    }
+
+    public function testSetRegion()
+    {
+        $data = " Pomorskie";
+        $this->cave->setRegion($data);
+        $this->assertSame($this->cave->getRegion($data), trim($data));
+    }
+
+    public function testSetCoordinatesWgs84()
+    {
+        $data = " λ: 18°27′04,80″, φ: 54°42′48,55″ ";
+        $this->cave->setCoordinatesWgs84($data);
+        $this->assertSame($this->cave->getCoordinatesWgs84($data), trim($data));
+    }
+
+    public function testSetCommunity()
+    {
+        $data = " Pomorskie ";
+        $this->cave->setCommunity($data);
+        $this->assertSame($this->cave->getCommunity($data), trim($data));
+    }
+
+    public function testSetCounty()
+    {
+        $data = " Czechy ";
+        $this->cave->setCounty($data);
+        $this->assertSame($this->cave->getCounty($data), trim($data));
+    }
+
+    public function testVoivodeship()
+    {
+        $data = " Małopolskie";
+        $this->cave->setVoivodeship($data);
+        $this->assertSame($this->cave->getVoivodeship($data), trim($data));
+    }
+
+    public function testSetOwner()
+    {
+        $data = " Nowy";
+        $this->cave->setOwner($data);
+        $this->assertSame($this->cave->getOwner($data), trim($data));
+    }
+
+    public function testSetBasisOfProtection()
+    {
+        $data = " Nowy";
+        $this->cave->setBasisOfProtection($data);
+        $this->assertSame($this->cave->getBasisOfProtection($data), trim($data));
+    }
+
+    public function testSetHoleExposure()
+    {
+        $data = " N";
+        $this->cave->setHoleExposure($data);
+        $this->assertSame($this->cave->getHoleExposure($data), trim($data));
+    }
+
+    public function testSetOtherHoles()
+    {
+        $data = " E";
+        $this->cave->setOtherHoles($data);
+        $this->assertSame($this->cave->getOtherHoles($data), trim($data));
+    }
+
+    public function testSetAbsoluteHeight()
+    {
+        $data = " 13";
+        $this->cave->setAbsoluteHeight($data);
+        $this->assertSame($this->cave->getAbsoluteHeight($data), trim($data));
+    }
+
+    public function testSetRelativeHeight()
+    {
+        $data = " 21";
+        $this->cave->setRelativeHeight($data);
+        $this->assertSame($this->cave->getRelativeHeight($data), trim($data));
+    }
+
+    public function testSetDepth()
+    {
+        $data = " 5";
+        $this->cave->setDepth($data);
+        $this->assertSame($this->cave->getDepth($data), trim($data));
+    }
+
+    public function testSetExceeds()
+    {
+        $data = " 4  ";
+        $this->cave->setExceeds($data);
+        $this->assertSame($this->cave->getExceeds($data), trim($data));
+    }
+
+    public function testSetDrop()
+    {
+        $data = " 42  ";
+        $this->cave->setDrop($data);
+        $this->assertSame($this->cave->getDrop($data), trim($data));
+    }
+
+    public function testSetLength()
+    {
+        $data = " 22";
+        $this->cave->setLength($data);
+        $this->assertSame($this->cave->getLength($data), trim($data));
+    }
+
+    public function testSetHorizontalExtension()
+    {
+        $data = " 41";
+        $this->cave->setHorizontalExtension($data);
+        $this->assertSame($this->cave->getHorizontalExtension($data), trim($data));
+    }
+
+    public function testSetGeographicalLocation()
+    {
+        $data = " Jaskinia jest usytuowana we wschodniej części miejscowości Puck";
+        $this->cave->setGeographicalLocation($data);
+        $this->assertSame($this->cave->getGeographicalLocation($data), trim($data));
+    }
+
+    public function testSetDescriptionOfAccess()
+    {
+        $data = " Jaskinia jest pod drogą wojewódzką S6";
+        $this->cave->setDescriptionOfAccess($data);
+        $this->assertSame($this->cave->getDescriptionOfAccess($data), trim($data));
+    }
+
+    public function testSetDescription()
+    {
+        $data = " Nowy opis tej jaskini    ";
+        $this->cave->setDescription($data);
+        $this->assertSame($this->cave->getDescription($data), trim($data));
+    }
+
+    public function testSetResearchHistory()
+    {
+        $data = " Nowy opis historii odkrycia tej jaskini ";
+        $this->cave->setResearchHistory($data);
+        $this->assertSame($this->cave->getResearchHistory($data), trim($data));
+    }
+
+    public function testSetDocumentationHistory()
+    {
+        $data = " Nowy opis historii badań i dokumentacji tej jaskini ";
+        $this->cave->setDocumentationHistory($data);
+        $this->assertSame($this->cave->getDocumentationHistory($data), trim($data));
+    }
+
+    public function testSetStatus()
+    {
+        $data = " Nie";
+        $this->cave->setStatus($data);
+        $this->assertSame($this->cave->getStatus($data), trim($data));
+    }
+
+    public function testSetLiterature()
+    {
+        $data = " Brak";
+        $this->cave->setLiterature($data);
+        $this->assertSame($this->cave->getLiterature($data), trim($data));
+    }
+
+    public function testSetStudyAuthors()
+    {
+        $data = " Jan Nowak, Stefek Nowak i Nowak Nowak";
+        $this->cave->setStudyAuthors($data);
+        $this->assertSame($this->cave->getStudyAuthors($data), trim($data));
+    }
+
+    public function testSetEditorial()
+    {
+        $data = " Zły brak bliźniak Jana Nowaka";
+        $this->cave->setEditorial($data);
+        $this->assertSame($this->cave->getEditorial($data), trim($data));
+    }
+
     protected function tearDown(): void
     {
         unset($this->cave);
