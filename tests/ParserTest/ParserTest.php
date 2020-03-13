19c4fbef03b73091302f31b7bc1f97c5c3ad8c40
Waldemar Graban <waldekgraban@gmail.com>
2020-03-13 13:09:16 +0100

    Add fixtures for Parser class tests

---
+++ tests/ParserTest/ParserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Waldekgraban\Scanner\Tests\Unit;
+
+use Waldekgraban\Scanner\Parser\Parser;
+use Waldekgraban\Scanner\Tests\TestCase;
+
+class ParserTest extends TestCase
+{
+    protected $parser;
+
+    protected function setUp(): void
+    {
+        //
+    }
+
+    protected function tearDown(): void
+    {
+        //
+    }
+}
